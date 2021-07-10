2b0274361917235e0d2d2b5ba4d501a4ecbe6bf4
Abiye <aabiye7@gmail.com>
2021-07-09 23:31:05 -0500

    update

--- user.js
+++ user.js
@@ -2,6 +2,8 @@ class User {
     constructor(username, password, age,location) {
         this.username = username;
         this.password = password;
+        this.age = age;
+        this.location = location
     
     }
     
